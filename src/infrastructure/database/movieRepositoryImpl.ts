5c64d16989fd1ae28d96ea2e862f70dd2d022851
dannycahyo <dannydwicahyono@gmail.com>
2024-02-14 21:04:35 +0700

    fix: delete unnecessary file

--- src/infrastructure/database/movieRepositoryImpl.ts
+++ src/infrastructure/database/movieRepositoryImpl.ts
@@ -16,7 +16,7 @@ import type {
   EnAbstractSchema,
 } from "../../domain/entities/movieEntity";
 import type { PersonWithRevenue } from "../../domain/entities/personEntity";
-import type { MovieRepository } from "../../domain/repositories/MovieRepository.1";
+import type { MovieRepository } from "../../domain/repositories/movieRepository";
 import type { MovieRequestParams } from "../../interfaces/controllers/types/movieRequestParams";
 import type { PaginationRequestParams } from "../../interfaces/controllers/types/paginationParams";
 import type { NewMovieDto } from "../../interfaces/dtos/movieDto";
